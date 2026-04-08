cb014d57e3337cf5002582fc101c5f5a445ed09f
S-Carmona <scarmonas@unicartagena.edu.co>
2026-04-07 23:30:18 -0500

    feat: mi primer commit real

---
+++ Ejemplo1.java
@@ -0,0 +1,8 @@
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        // Esto es programación procedural (paso a paso)
+        String nombre = "sasha";
+        System.out.println("Hola, mi nombre es: " + nombre);
+        System.out.println("Esto es taller de poo");
+    }
+}
